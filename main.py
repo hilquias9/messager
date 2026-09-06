6d56569e5ed19b63ed35c1a2740071fb57ab445c
hilquias9 <hilquiasazevedo9@gmail.com>
2026-09-06 11:52:26 -0300

    criando a classe e funcao para enviar mensagens

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
+from secrets import token_hex
 class Messager:
-    pass
+    def __init__(self,*headers:str):
+        self.headers={"length":token_hex(8).encode(),"end_length":token_hex(8).encode()}
+        for header in headers:
+            self.headers[header]=token_hex(8).encode()
+            self.headers[f"end_{header}"]=token_hex(8).encode()
+
+    def unpackger(self,msg:bytes):
+        pass
+
+    def m_packger(self,msg:bytes,header): #message
+        msg_length=str(len(msg)+32+(len(msg)+32)).encode()
+        message=self.headers[header]+self.headers["length"]+msg_length+self.headers["end_length"]+msg+self.headers[f"end_{header}"]
+        return message
+
+    def f_packger(self,msg:bytes): #file
+        pass
+
+    def see_headers(self):
+        return self.headers
+
